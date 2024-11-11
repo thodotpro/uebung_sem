c96a0065fda25b8b81dc55830977551c012c928e
Angie Zinterl <01430236@edu.campus02.at>
2024-11-11 14:16:47 +0100

    Steuer dummy added

--- uebung_sem/main.py
+++ uebung_sem/main.py
@@ -1,1 +1,10 @@
 artikel = [['schuhe', 100, 20], ['iphone', 1000, 10], ['ipad', 800, 5], ['airpods', 300, 15], ['kugelschreiber', 2, 20]]
+
+
+
+# steuer funktion
+
+def steuer():
+    for i in artikel:
+        print(i)
+
