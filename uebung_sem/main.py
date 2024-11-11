10dbeccedd4e871072aa64bfb61aa3ffb3a684f4
Thomas Proksch <tho.pro@posteo.org>
2024-11-11 14:27:50 +0100

    added comments

--- uebung_sem/main.py
+++ uebung_sem/main.py
@@ -1,15 +1,16 @@
 artikel = [['schuhe', 100, 20], ['iphone', 1000, 10], ['ipad', 800, 5], ['airpods', 300, 15], ['kugelschreiber', 2, 20]]
 
+#rabatt funktion
 def rabatt():
     for i in artikel:
         print(i)
 
 # steuer funktion
-
 def steuer():
     for i in artikel:
         print(i)
 
 rabatt()
+steuer()
 
-def main():
+def summe():
