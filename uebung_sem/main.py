0dae056d383b9ece29283ac66e17d0bddfd721df
thodotpro <tho.pro@posteo.org>
2024-11-23 17:34:17 +0100

    2024-11-23

--- uebung_sem/main.py
+++ uebung_sem/main.py
@@ -1,7 +1,26 @@
 artikel = [['schuhe', 100, 20], ['iphone', 1000, 10], ['ipad', 800, 5], ['airpods', 300, 15], ['kugelschreiber', 2, 20]]
 
-def rabatt():
-    for i in artikel:
-        print(i)
+discount = 0
 
-rabatt()
+def discount():
+    for item in artikel:
+        if item[2]>=5:
+            discount = 0.05
+            return discount
+        elif item[2] >= 10:
+            discount = 0.10
+            return discount
+
+discount()
+
+
+
+
+
+
+
+
+
+#Pro Artikel gibt es einen 5% igen Rabatt ab 5 Stück des gleichen Artikels, bzw. 10%
+#Rabatt ab 10 Stück eines Artikels – der Rabatt wird individuell für jeden Artikel
+#berechnet.
