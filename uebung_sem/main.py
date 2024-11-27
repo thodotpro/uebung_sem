61f557f80c7a022df5fba9a96af80caaab74a98f
thodotpro <tho.pro@posteo.org>
2024-11-27 19:03:17 +0100

    2024-11-23

--- uebung_sem/main.py
+++ uebung_sem/main.py
@@ -1,9 +1,17 @@
 artikel = [['schuhe', 100, 20], ['iphone', 1000, 10], ['ipad', 800, 5], ['airpods', 300, 15], ['kugelschreiber', 2, 20]]
 
 #rabatt funktion
-def rabatt():
-    for i in artikel:
-        print(i)
+
+def discount():
+    for item in artikel:
+        if item[2] >= 5:
+            discount = 0.05
+            return discount
+        elif item[2] >= 10:
+            discount = 0.10
+            return discount
+
+discount()
 
 # steuer funktion
 def steuer():
